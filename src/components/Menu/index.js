f7706aafcae89e305390d5d61e0c83fc4d43abba
vellhig <higor.velloso@technipfmc.com>
2020-08-01 21:27:07 -0300

    Commit da Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/Img/Logo.png';
-import './Menu.css'
-import ButtonLink from './components/ButtonLink'
+import './Menu.css';
+import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <header>
             <nav className="Menu">
-                <a href="/">
+                <Link to="/">
                     <img src={Logo} className="Logo" alt="HigorFlix logo" />
-                </a>
+                </Link>
 
-                <ButtonLink className="ButtonLink" href="/">
+                <ButtonLink as={Link} className="ButtonLink" to="/cadastro/video">
                     Novo vídeo
                 </ButtonLink>
             </nav>
